4fc9d452cce742f0012e56c5f6180f32ae5317ba
Jorge García <goeknax@hotmail.com>
2023-04-17 19:15:04 -0600

    Test with cypress

---
+++ assets/components/History.cy.js
@@ -0,0 +1,16 @@
+import History from './History.vue'
+
+describe('<History />', () => {
+  it('renders', () => {
+    // see: https://on.cypress.io/mounting-vue
+    cy.mount(History)
+  })
+  it('carga pagina con cero información', () => {
+    cy.mount(History)
+    cy.contains('div', 'No hay información.')
+  })
+  it('carga pagina algunos datos', () => {
+    cy.mount(History)
+    cy.get('.list-group').find('a').should('have.length', 7)
+  })
+})
